4b0116e7e4807a777f0419305243ebc197e59c46
Karthik <96225312+svkarthik86@users.noreply.github.com>
2022-09-14 17:00:00 +0530

    Assignment problem1 solution

---
+++ Cprogram/welcome.c
@@ -0,0 +1,4 @@
+#include 'stdio.h'
+void main(){
+printf(%s,"Welcome to C Programming")
+}
---
+++ FindEvenOdd.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class FindEvenOdd{
+public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int number=sc.nextInt();
+    //int number=33;
+    if( number%2==0){
+        System.out.println("EVEN");
+    }
+    else{System.out.println("ODD");}
+    
+
+}}
